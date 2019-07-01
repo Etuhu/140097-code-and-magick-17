f67ea977fb30bef7e6a5e15d857be252b2967597
Etuhu <andgiz@mail.ru>
2019-07-02 00:12:11 +0700

    Выполнено задание 9. Учебный проект: в движении

--- js/setup.js
+++ js/setup.js
@@ -7,9 +7,14 @@ var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var FIREBALLS_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
+
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var userDialog = document.querySelector('.setup');
+var defaultCoordsUserDialog = {
+  x: userDialog.style.left,
+  y: userDialog.style.top
+};
 var fragment = document.createDocumentFragment();
 var mainWizard = document.querySelector('.setup-wizard');
 var mainCoatColor = mainWizard.querySelector('.wizard-coat');
@@ -20,11 +25,22 @@ var eyesColorInput = document.querySelector('input[name="eyes-color"]');
 var fireballsColorInput = document.querySelector('input[name="fireball-color"]');
 var usernameInput = document.querySelector('input[name="username"]');
 
+var bagWithArtifacts = document.querySelector('.setup-artifacts');
+var starArtifact = document.querySelector('#draggedStar');
+
+var dialogHandle = userDialog.querySelector('.upload');
+
 var setupOpenButton = document.querySelector('.setup-open');
 var setupCloseButton = userDialog.querySelector('.setup-close');
 
 var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+};
+
+// Устанавливает координаты расположения окна настроек персонажа по умолчанию
+var setDefaultCoordsUserDialog = function () {
+  userDialog.style.left = defaultCoordsUserDialog.x;
+  userDialog.style.top = defaultCoordsUserDialog.y;
 };
 
 // Открытие/закрытие окна настройки персонажа
@@ -44,11 +60,13 @@ var onPopupEscPress = function (evt) {
 var openPopup = function () {
   userDialog.classList.remove('hidden');
   document.addEventListener('keydown', onPopupEscPress);
+  setDefaultCoordsUserDialog();
 };
 
 var closePopup = function () {
   userDialog.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
+  setDefaultCoordsUserDialog();
 };
 
 setupOpenButton.addEventListener('click', function () {
@@ -69,6 +87,54 @@ setupCloseButton.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     closePopup();
   }
+});
+
+// Добавляет возможность перетаскивания окна настроек персонажа
+dialogHandle.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+    userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (prvDefEvt) {
+        prvDefEvt.preventDefault();
+        dialogHandle.removeEventListener('click', onClickPreventDefault);
+      };
+      dialogHandle.addEventListener('click', onClickPreventDefault);
+    }
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 });
 
 // Изменение цвета мантии персонажа
@@ -128,3 +194,32 @@ var drawingWizards = function () {
 
 drawingWizards();
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+// Добавление функции перетаскивания предметов из магазина в рюкзак
+var dragStart = function (evt) {
+  evt.dataTransfer.effectAllowed = 'move';
+  evt.dataTransfer.setData('Text', evt.target.getAttribute('id'));
+  evt.dataTransfer.setDragImage(evt.target, 32, 25);
+  return true;
+};
+
+var dragEnter = function (evt) {
+  evt.preventDefault();
+  return true;
+};
+
+var dragOver = function (evt) {
+  evt.preventDefault();
+};
+
+var dragDrop = function (evt) {
+  var data = evt.dataTransfer.getData('Text');
+  evt.target.appendChild(document.getElementById(data));
+  evt.stopPropagation();
+  return false;
+};
+
+starArtifact.addEventListener('dragstart', dragStart);
+bagWithArtifacts.addEventListener('dragenter', dragEnter);
+bagWithArtifacts.addEventListener('dragover', dragOver);
+bagWithArtifacts.addEventListener('drop', dragDrop);
