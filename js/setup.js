00568fd28f6b466764e4c7dfd5c5e598deda337e
Etuhu <andgiz@mail.ru>
2019-07-10 03:20:19 +0700

    Выполнено задание 13. Учебный проект: pimp my mage

--- js/setup.js
+++ js/setup.js
@@ -1,32 +1,36 @@
 'use strict';
 
-var FIREBALLS_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+(function () {
+  var FIREBALLS_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var mainWizard = document.querySelector('.setup-wizard');
-var mainCoatColor = mainWizard.querySelector('.wizard-coat');
-var mainEyesColor = mainWizard.querySelector('.wizard-eyes');
-var fireballsColor = document.querySelector('.setup-fireball-wrap');
-var coatColorInput = document.querySelector('input[name="coat-color"]');
-var eyesColorInput = document.querySelector('input[name="eyes-color"]');
-var fireballsColorInput = document.querySelector('input[name="fireball-color"]');
+  var mainWizard = document.querySelector('.setup-wizard');
+  var mainCoatColor = mainWizard.querySelector('.wizard-coat');
+  var mainEyesColor = mainWizard.querySelector('.wizard-eyes');
+  var fireballsColor = document.querySelector('.setup-fireball-wrap');
+  var coatColorInput = document.querySelector('input[name="coat-color"]');
+  var eyesColorInput = document.querySelector('input[name="eyes-color"]');
+  var fireballsColorInput = document.querySelector('input[name="fireball-color"]');
 
-// Изменение цвета мантии персонажа
-mainCoatColor.addEventListener('click', function () {
-  coatColorInput.value = window.setup.COAT_COLORS[window.util.getRandom(1, window.setup.COAT_COLORS.length)];
-  var mainCoatColorValue = coatColorInput.value;
-  mainCoatColor.style.fill = mainCoatColorValue;
-});
+  // Изменение цвета мантии персонажа
+  mainCoatColor.addEventListener('click', function () {
+    coatColorInput.value = COAT_COLORS[window.util.getRandom(1, COAT_COLORS.length)];
+    var mainCoatColorValue = coatColorInput.value;
+    mainCoatColor.style.fill = mainCoatColorValue;
+  });
 
-// Изменение цвета глаз персонажа
-mainEyesColor.addEventListener('click', function () {
-  eyesColorInput.value = window.setup.EYES_COLORS[window.util.getRandom(1, window.setup.EYES_COLORS.length)];
-  var mainEyesColorValue = eyesColorInput.value;
-  mainEyesColor.style.fill = mainEyesColorValue;
-});
+  // Изменение цвета глаз персонажа
+  mainEyesColor.addEventListener('click', function () {
+    eyesColorInput.value = EYES_COLORS[window.util.getRandom(1, EYES_COLORS.length)];
+    var mainEyesColorValue = eyesColorInput.value;
+    mainEyesColor.style.fill = mainEyesColorValue;
+  });
 
-// Изменение цвета файерболлов
-fireballsColor.addEventListener('click', function () {
-  fireballsColorInput.value = FIREBALLS_COLORS[window.util.getRandom(1, FIREBALLS_COLORS.length)];
-  var fireballsColorValue = fireballsColorInput.value;
-  fireballsColor.style.backgroundColor = fireballsColorValue;
-});
+  // Изменение цвета файерболлов
+  fireballsColor.addEventListener('click', function () {
+    fireballsColorInput.value = FIREBALLS_COLORS[window.util.getRandom(1, FIREBALLS_COLORS.length)];
+    var fireballsColorValue = fireballsColorInput.value;
+    fireballsColor.style.backgroundColor = fireballsColorValue;
+  });
+})();
